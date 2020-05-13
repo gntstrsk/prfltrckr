094b3f34d48c63ea1bffa81c0a807d87c0cd7b50
gntstrsk <56361733+gntstrsk@users.noreply.github.com>
2020-05-13 08:46:38 +0800

    Added database connection (mongoose for mongo).

--- app.js
+++ app.js
@@ -8,27 +8,43 @@ const app = express();
 const routes = require('./routes');
 const morgan = require('morgan');
 
-//app.set('view engine', 'ejs');
-//app.use(express.static('views/css'));
+// Database Initialization
+const db = require('mongoose');
+const dbName = 'prfltrckr-test'; // prfltrckr
 
-app.use(morgan(process.env.ENV));
-app.use(express.json());
-
-app.use('/', routes);
-
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-        error: {
-        	message: err.message
-        }
-    });
+//TODO: save mongo urls to env vars
+db.connect('mongodb://localhost:27017/' + dbName, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-module.exports = app;
+var connection = db.connection;
+connection.on('error', console.error.bind(console, 'Database Connection Error:'));
+connection.once('open', function() {
 
-if (process.env.ENV == 'dev' || process.env.ENV == 'prod') {
-    const server = http.createServer(app);
-    server.listen(process.env.PORT);
-    console.log(process.env.PORT);
-}
+    console.log("Connected successfully to database!");
+    //app.set('view engine', 'ejs');
+    //app.use(express.static('views/css'));
+
+    app.use(morgan(process.env.ENV));
+    app.use(express.json());
+
+    app.use('/', routes);
+
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.json({
+            error: {
+                message: err.message
+            }
+        });
+    });
+
+    module.exports = app;
+
+    if (process.env.ENV == 'dev' || process.env.ENV == 'prod') {
+        const server = http.createServer(app);
+        server.listen(process.env.PORT);
+    }
+
+});
