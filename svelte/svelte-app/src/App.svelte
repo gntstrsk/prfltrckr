b1acb0689e667a38c763109a6e9de89f4b1be99a
gntstrsk <56361733+gntstrsk@users.noreply.github.com>
2021-02-13 07:46:16 +0800

    Testing svelte with a bunch of super secret pages.

---
+++ svelte/svelte-app/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import {tick} from 'svelte';
+	import Index from './Index.svelte';
+	import ListView from './ListView.svelte';
+	import TileView from './TileView.svelte';
+	import ProfileView from './ProfileView.svelte';
+
+	const views = ["Index", "ListView", "TileView", "ProfileView"];
+	let currentView = Index;
+
+	const code = "asterisk";
+	let codeHolder = "";
+	async function handleKeydown(e) {
+
+		if (code.includes(e.key)) {
+			e.preventDefault();
+
+			codeHolder += e.key;
+
+			if(codeHolder == code){
+				currentView = ListView;
+			}
+			else if(codeHolder !== code.substring(0, codeHolder.length)) {
+				codeHolder = "";
+			}
+		} else {
+			codeHolder = "";
+		}
+
+		console.log(codeHolder);
+	}
+
+</script>
+
+<svelte:window on:keydown={handleKeydown}/>
+
+<svelte:component this={currentView}/>
+
